69ac9a52d3457b42c051ddec75466a20d315e705
tboyd4 <tboyd1898@gmail.com>
2020-04-19 15:10:21 -0500

    turned app.js into a class component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
