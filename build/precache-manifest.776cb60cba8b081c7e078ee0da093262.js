1bcfac25a05a93a886695b6277e44faecd58ea98
tboyd4 <tboyd1898@gmail.com>
2020-04-19 14:57:10 -0500

    initial build commit

---
+++ build/precache-manifest.776cb60cba8b081c7e078ee0da093262.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867e21e74ff1532a62b25cf3c77f85dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2119eecbcdfd4eccec8d",
+    "url": "/static/css/main.5e3647ae.chunk.css"
+  },
+  {
+    "revision": "139b0ca57f2a3d531835",
+    "url": "/static/js/2.761ad81e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.761ad81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2119eecbcdfd4eccec8d",
+    "url": "/static/js/main.34560b1b.chunk.js"
+  },
+  {
+    "revision": "83831abfa7fe2fd0d657",
+    "url": "/static/js/runtime-main.109bbbc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
