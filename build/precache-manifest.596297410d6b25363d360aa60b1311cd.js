cf5098bf28be8fb280eb773de02b402476fe29e7
tboyd4 <tboyd1898@gmail.com>
2020-04-19 18:57:48 -0500

    build3

---
+++ build/precache-manifest.596297410d6b25363d360aa60b1311cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadadede982d79005bfd830a1a85286a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b420fb80edc21b89dcfb",
+    "url": "/static/css/main.7911ddbf.chunk.css"
+  },
+  {
+    "revision": "443cf711f63b7974c356",
+    "url": "/static/js/2.bc3a8c8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc3a8c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b420fb80edc21b89dcfb",
+    "url": "/static/js/main.bb7443a6.chunk.js"
+  },
+  {
+    "revision": "83831abfa7fe2fd0d657",
+    "url": "/static/js/runtime-main.109bbbc1.js"
+  }
+]);
